e6ff2e8fb0840b71d95f09548ab0cb8cb92c1bb4
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2024-03-22 06:16:51 +0900

    FEAT: Edited experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -18,7 +18,7 @@ const Experience = () => {
             className={activeTab === 1 ? 'active' : null}
             onClick={() => toggleTab(1)}
           >
-            HeadHunter
+            soffhub.uz
           </button>
           <button
             className={activeTab === 2 ? 'active' : null}
@@ -30,23 +30,38 @@ const Experience = () => {
         <div className="detail">
           <div className={`content ${activeTab === 2 ? 'active' : ''}`}>
             <h3>
-              Online Mentor at <span>@foxminded</span>
+              Online Mentor at{' '}
+              <span>
+                <a href="https://soffhub.uz/">@foxminded</a>
+              </span>
             </h3>
-            <h5>Jan 2023 - Aug 2023</h5>
+            <h5>Jul 2023 - Dec 2023</h5>
             <p>
-              Helped students understand best practices in frontend development,
-              including HTML, CSS, and JavaScript standards.
+              Developed dynamic web apps using MVC architecture for efficient
+              code organization and scalability.
             </p>
+            <p>Converted Figma / PSD to responsive and pixel-perfect design.</p>
             <p>
-              Prepared illustrations, materials, tasks and docs for easy
-              learning.
+              Created reusable and generic UI Components using SOLID Principles.
+            </p>
+            <p>Worked with UI component libraries such as Material UI.</p>
+            <p>Integrated third party payment gateways into the web app.</p>
+            <p>
+              Applied scalable user authentication using JWT, session cookies,
+              and bcrypt.
+            </p>
+            <p>Created API endpoints and database queries with PostgresQL.</p>
+            <p>Real-time user interaction with Socket.io</p>
+            <p>
+              Implemented GraphQL schemas to efficiently query and manipulate
+              data.
             </p>
           </div>
           <div className={`content ${activeTab === 1 ? 'active' : ''}`}>
             <h3>
               <em>Front-end Developer | Freelancing</em>
             </h3>
-            <h5>Jul 2023 - Nov 2023</h5>
+            <h5>Jan 2023 - Jul 2023</h5>
             <p>
               Developed reusable components and templates to streamline the
               development process.
